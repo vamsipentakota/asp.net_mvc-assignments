c44d8f793ef7242bfe52888cbad75ce3f4279616
Vamsi Pentakota <v.pentakota@zensar.com>
2022-07-09 11:30:46 +0530

    final project azure

---
+++ azure final project/miniproject/miniproject/Data/miniprojectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using miniproject.Model;
+
+namespace miniproject.Data
+{
+    public class miniprojectContext : DbContext
+    {
+        public miniprojectContext (DbContextOptions<miniprojectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<miniproject.Model.Product> Product { get; set; }
+    }
+}
